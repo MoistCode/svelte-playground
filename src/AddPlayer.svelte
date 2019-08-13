a9f07cdfa8ba8214bac76846287f5556c3fb3893
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-12 21:04:42 -0700

    add player

---
+++ src/AddPlayer.svelte
@@ -0,0 +1,12 @@
+<script>
+  let player = {
+    name: "",
+    points: 0
+  };
+</script>
+
+<form class="grid-3">
+  <input type="text" placeholder="Player Name" bind:value={player.name} />
+  <input type="number" placeholder="Player Points" bind:value={player.points} />
+  <input type="submit" class="btn btn-primary" value="Add Player" />
+</form>
--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
 
   const players = [
     {
@@ -24,6 +25,7 @@
 
 <Navbar />
 <div class="container">
+  <AddPlayer />
   {#if players.length === 0}
     <p>No Players</p>
   {:else}
